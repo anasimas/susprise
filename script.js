e327d99711b0f41391627038fe01deccaeb04660
anasimas <ana.simas2109@outlook.com>
2024-06-11 21:13:49 -0300

    fim

--- script.js
+++ script.js
@@ -1,64 +1,43 @@
 const botaoEnviar = document.getElementById('enviar');
-const botaoCancelar = document.getElementById('limpar');
+const botaoVoltar = document.getElementById('voltar');
 
-botaoEnviar.addEventListener("click", async () => {
-  let prompt = promptBuilder();
-  console.log(prompt);
-  await sendMessage(prompt);
-});
-
-async function sendMessage(prompt) {
-  if (!prompt) return;
-  try {
-    const response = await fetch("https://api.openai.com/v1/chat/completions", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer sk-proj-35X6lOMKBMTFAZV5B2nVT3BlbkFJ0LrIT6c5sAwLOEfhkD5p",
-      },
-      body: JSON.stringify({
-        model: "gpt-3.5-turbo",
-        prompt: prompt
-      })
-    });
-    if (!response.ok) {
-      throw new Error('Erro ao fazer a requisição para o ChatGPT');
-    }
-    const data = await response.json();
-    const dados = data.choices[0].text.trim();
-    console.log(dados);
-  } catch (error) {
-    console.log(error);
-    swal("Ops!", "Ocorreu algum problema para obter a resposta! Tente novamente", "warning");
-  }
-}
-
-function promptBuilder() {
-  let valor = document.getElementById('opt_1').value;
-  if (valor) {
-    valor = 'econômico';
-  }
-  let horario = document.getElementById('opt_2').value;
-  let refeicao = document.getElementById('opt_3').value;
-  let opt3;
-  if (refeicao == 1) {
-    opt3 = 'com refeição'
+function enviar() {
+  let parametro = document.getElementById('opt_1').value;
+  if(parametro == 1){
+    document.getElementById('container').innerHTML = `
+      <div class="header d-flex flex-column justify-content-center align-items-center">
+        <img src="img/img.jpg">
+      </div>
+      <h1>Resposta errada >:c</h1>
+      <div class="buttons d-flex justify-content-center align-items-center">
+        <button id="voltar" onclick="voltar()">Voltar</button>
+      </div>
+    `
   } else {
-    opt3 = 'sem refeição'
+    document.getElementById('input').innerHTML =
+      `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/6hdVWrV8WNCZRLUHU7Mz99?utm_source=generator&theme=0" width="100%" height="400" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+      <div class="buttons d-flex justify-content-center align-items-center">
+        <button id="voltar" onclick="voltar()">Voltar</button>
+      </div>`
   }
-  let local = document.getElementById('opt_4').value;
-  let opt4;
-  if (local == 1) {
-    opt4 = 'externamente';
-  } else {
-    opt4 = 'em local fechado';
-  }
-  let qtd = document.getElementById('qtd_atv').value;
-  if (!qtd) {
-    swal("Ops!", "Insira um valor, ou você não quer fazer nada comigo? :(", "warning")
-  };
 
-  let prompt = `Finja que sou sua namorada e me sugira ${qtd} ideia(s) de encontro(s) ${valor}, de ${horario}, ${opt3}, que seja(m) ${opt4}`
-
-  return prompt;
 };
+
+function voltar() {
+    document.getElementById('container').innerHTML = `
+        <div class="header d-flex flex-column justify-content-center align-items-center">
+            <h1>te amo!</h1>
+            <p>Feliz aniversário de noivado e dia dos namorados!</p>
+        </div>
+        <div class="input d-flex flex-column justify-content-center align-items-center" id="input">
+            <label for="opt_1">2 reais ou um presente misterioso?</label>
+            <select name="opt_1" id="opt_1">
+                <option value="1">2 reais</option>
+                <option value="2">Presente</option>
+            </select>
+            <div class="buttons d-flex justify-content-center align-items-center">
+                <button id="enviar" onclick="enviar()"><i class="fa-solid fa-heart"></i></button>
+            </div>
+        </div>
+    `
+  }
