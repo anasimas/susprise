3712432bd1a00dc3a3b9f5ef550db5f97d4e9085
ANA CAROLINA LAMBERTUCCI SIMAS <ana.simas@ielusc.br>
2024-06-14 13:16:07 -0300

    suavização das transições

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function enviar() {
+  let container = document.getElementById('container');
   let parametro = document.getElementById('opt_1').value;
   if (parametro == 1) {
     swal({
@@ -10,48 +11,70 @@ function enviar() {
       imageAlt: "nova"
     });
   } else {
-    document.getElementById('spotifyPlayer').className = 'input d-flex flex-column justify-content-center align-items-center';
-    document.getElementById('spotifyPlayer').style.display = 'block';
-    document.getElementById('spotifyPlayer').style.opacity = '1';
-    document.getElementById('input').style.display = 'none';
-    document.getElementById('input').className = 'none'; //tira o display flex do bootstrap
+    container.style.opacity = '0'
+    setTimeout(function () {
+      document.getElementById('spotifyPlayer').className = 'input d-flex flex-column justify-content-center align-items-center';
+      document.getElementById('spotifyPlayer').style.display = 'block';
+      document.getElementById('spotifyPlayer').style.opacity = '1';
+      document.getElementById('input').style.display = 'none';
+      document.getElementById('input').className = 'none'; //tira o display flex do bootstrap
 
-    document.getElementById('texto').innerText = "Esse é o mini disco virtual! Um presente com músicas que me lembrarm bons momentos nossos, por diferentes razões. Você é especial demais."
+      document.getElementById('texto').innerText = "Esse é o mini disco virtual! Um presente com músicas que me lembram bons momentos nossos, por diferentes razões. Você é especial demais."
+      setTimeout(function () {
+        container.style.opacity = '1'
+      }, 100)
+    }, 500)
   }
 };
 
 function voltar() {
-  document.getElementById('spotifyPlayer').className = '';
-  document.getElementById('spotifyPlayer').style.display = 'none';
-  document.getElementById('spotifyPlayer').style.opacity = '0';
-  document.getElementById('input').style.display = 'block';
-  document.getElementById('input').className = 'input d-flex flex-column justify-content-center align-items-center';
-  document.getElementById('texto').innerText = "Feliz aniversário de noivado e dia dos namorados!"
+  let container = document.getElementById('container');
+  container.style.opacity = '0'
+  setTimeout(function () {
+    document.getElementById('spotifyPlayer').className = '';
+    document.getElementById('spotifyPlayer').style.display = 'none';
+    document.getElementById('spotifyPlayer').style.opacity = '0';
+    document.getElementById('input').style.display = 'block';
+    document.getElementById('input').className = 'input d-flex flex-column justify-content-center align-items-center';
+    document.getElementById('texto').innerText = "Feliz aniversário de noivado e dia dos namorados!"
+
+    setTimeout(function () {
+      container.style.opacity = '1'
+    }, 100)
+  }, 500)
 }
 
 let select = document.getElementById('opt_2');
 
-function seletor(element) {
-  console.log(element);
-
+function seletor(value) {
+  let element = Number(value);
+  const player = document.getElementById('player');
+  player.style.opacity = '0';
+  setTimeout(function () {
+    switch (element) {
+      case 1:
+        player.setAttribute("src", 'https://open.spotify.com/embed/playlist/6hdVWrV8WNCZRLUHU7Mz99?utm_source=generator');
+        break;
+      case 2:
+        player.setAttribute("src", 'https://open.spotify.com/embed/playlist/4KIBvaVQHnbhD7BVXGNn23?utm_source=generator'); // romanticas
+        break;
+      case 3:
+        player.setAttribute("src", 'https://open.spotify.com/embed/playlist/49LUzMSXqUzvqamVgMVjc0?utm_source=generator'); // espaço
+        break;
+      case 4:
+        player.setAttribute("src", 'https://open.spotify.com/embed/playlist/3cz70PGZE26qE6mlswHnqs?utm_source=generator'); // cantar no carro
+        break;
+      case 5:
+        player.setAttribute("src", 'https://open.spotify.com/embed/playlist/1vsPbo3RcXK2pE4pprB1V5?utm_source=generator'); // nossas divas pops
+        break;
+      default:
+        console.log("Valor inválido");
+    }
+    setTimeout(function () {
+      player.style.opacity = '1';
+    }, 100);
+  }, 500);
 }
 
 
 
-
-// switch (element) {
-//   case 1:
-//     document.getElementById('player').setAttribute("src", 'https://open.spotify.com/embed/playlist/6hdVWrV8WNCZRLUHU7Mz99?utm_source=generator')
-//     break;
-//   case 2:
-//     document.getElementById('player').setAttribute("src", 'https://open.spotify.com/embed/playlist/4KIBvaVQHnbhD7BVXGNn23?utm_source=generator'); //romanticas
-//     break;
-//   case 3:
-//     document.getElementById('player').setAttribute("src", 'https://open.spotify.com/embed/playlist/49LUzMSXqUzvqamVgMVjc0?utm_source=generator'); //espaço
-//     break;
-//   case 4:
-//     document.getElementById('player').setAttribute("src", 'https://open.spotify.com/embed/playlist/3cz70PGZE26qE6mlswHnqs?utm_source=generator'); //cantar no carro
-//     break;
-//   case 5:
-//     document.getElementById('player').setAttribute("src", 'https://open.spotify.com/embed/playlist/1vsPbo3RcXK2pE4pprB1V5?utm_source=generator'); //cantar no carro
-// }
